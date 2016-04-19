e0356cd43a13d06076d56ba3af9dcd9df1037291
Todd Fleming <tbfleming@gmail.com>
2016-04-18 20:06:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+window.WrapVirtualDom = {
+    h: require('virtual-dom/h'),
+    diff: require('virtual-dom/diff'),
+    patch: require('virtual-dom/patch'),
+    createElement: require('virtual-dom/create-element'),
+};
